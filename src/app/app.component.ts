6cbb60474ed5daecd2d8ae3958edeb30e1942dae
Vidhyadharan D <it.vidhyadharan@gmail.com>
2018-10-08 15:01:54 +0530

    Delayed the chart renderer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,63 +10,67 @@ import { WeatherService } from './weather.service';
 export class AppComponent implements OnInit {
   title = 'charts';
   chart = []; // This will hold our charts info
-  chartIds = ['canvas', 'canvas2']; // this will hold the chart canvas id
+  chartIds = []; // this will hold the chart canvas id
 
   constructor(private _weather: WeatherService, public render2: Renderer2) {}
   public ngOnInit() {
     this._weather.getJSON().subscribe(res => {
-      // console.log(res);
+      this.chartIds = ['canvas', 'canvas2'];
 
-      let temp_max = res['list'].map(reslist => reslist.main.temp_max);
-      let temp_min = res['list'].map(reslist => reslist.main.temp_min);
-      let alldates = res['list'].map(reslist => reslist.dt);
+      // Delayed the chart renderer
 
-      let weatherDates = [];
-      alldates.forEach(iter => {
-        let jsdate = new Date(iter * 1000);
-        weatherDates.push(
-          jsdate.toLocaleTimeString('en', {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-          })
-        );
-      });
-      this.chart = new Chart('canvas', {
-        type: 'line',
-        data: {
-          labels: weatherDates,
-          datasets: [
-            {
-              data: temp_max,
-              borderColor: '#3cba9f',
-              fill: false
-            },
-            {
-              data: temp_min,
-              borderColor: '#ffcc00',
-              fill: false
-            }
-          ]
-        },
-        options: {
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [
+      setTimeout(() => {
+        let temp_max = res['list'].map(reslist => reslist.main.temp_max);
+        let temp_min = res['list'].map(reslist => reslist.main.temp_min);
+        let alldates = res['list'].map(reslist => reslist.dt);
+
+        let weatherDates = [];
+        alldates.forEach(iter => {
+          let jsdate = new Date(iter * 1000);
+          weatherDates.push(
+            jsdate.toLocaleTimeString('en', {
+              year: 'numeric',
+              month: 'short',
+              day: 'numeric'
+            })
+          );
+        });
+        this.chart = new Chart('canvas', {
+          type: 'line',
+          data: {
+            labels: weatherDates,
+            datasets: [
               {
-                display: true
-              }
-            ],
-            yAxes: [
+                data: temp_max,
+                borderColor: '#3cba9f',
+                fill: false
+              },
               {
-                display: true
+                data: temp_min,
+                borderColor: '#ffcc00',
+                fill: false
               }
             ]
+          },
+          options: {
+            legend: {
+              display: false
+            },
+            scales: {
+              xAxes: [
+                {
+                  display: true
+                }
+              ],
+              yAxes: [
+                {
+                  display: true
+                }
+              ]
+            }
           }
-        }
-      });
+        });
+      }, 1000);
     });
   }
 }
--- src/app/chart.render.directive.ts
+++ src/app/chart.render.directive.ts
@@ -4,17 +4,17 @@ import { Directive, ElementRef, OnInit, Renderer2, Input } from '@angular/core';
   selector: '[appChartRender]'
 })
 export class ChartRenderDirective implements OnInit {
-  @Input('appChartRender')
-  appChartRender: string[];
+  // @Input('appChartRender')
+  // appChartRender: string[];
 
   constructor(private el: ElementRef, private renderer2: Renderer2) {}
 
   ngOnInit() {
-   this.appendEmptyCanvas();
+   this.appendEmptyCanvas(this.getTheIdList());
   }
 
-  appendEmptyCanvas() {
-    for (const str of this.getTheIdList()) {
+  appendEmptyCanvas(idList: string[]) {
+    for (const str of idList) {
       const canvasElement = this.renderer2.createElement('canvas');
       this.renderer2.setProperty(canvasElement, 'id', str);
       this.renderer2.appendChild(this.el.nativeElement, canvasElement);
@@ -22,11 +22,16 @@ export class ChartRenderDirective implements OnInit {
   }
   getTheIdList(): string[] {
     let idList: string[];
+    idList = new Array<string>();
     if (!this.appChartRender) {
       idList = new Array<string>();
-    } else if (this.appChartRender && this.appChartRender.length > 0) {
+    } else if (this.appChartRender && this.appChartRender.length >= 0) {
       idList = this.appChartRender;
     }
     return idList;
+  }
+
+  @Input() set appChartRender ( appChartRender: string[]) {
+    this.appendEmptyCanvas(appChartRender);
   }
 }
